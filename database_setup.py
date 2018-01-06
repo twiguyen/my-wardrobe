742d9dc299bc5f927703c55c136ae8220e79b2c3
Twiggy <JenniferTPNguyen@gmail.com>
2018-01-05 22:32:34 -0500

    Added password hash database

--- database_setup.py
+++ database_setup.py
@@ -2,9 +2,9 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
-
 
 class User(Base):
     __tablename__ = 'user'
@@ -14,6 +14,21 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
+
+class Password(Base):
+    __tablename__ = 'password'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
 class ClothesType(Base):
     __tablename__ = 'clothesType'
